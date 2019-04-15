6376e8f1ac9a26e69a8747f27427edd308d70378
tryn0 <vkae@live.com>
2019-04-15 22:01:00 +0200

    0.02a

--- README.md
+++ README.md
@@ -7,9 +7,5 @@ Un código JavaFX simple para este trabajo, con múltiples commits, branchs con 
 Hará un programa JavaFX que introduzcas tu información y te la muestre por la línea de comandos donde ejecutaste el programa.
 
 ### Versión
-El branch 0.01 tiene el programa y commits del código de JavaFX de la versión 0.01x, título y cajas de texto para introducir nombre y apellidos con su correspondiente botón para aceptar, al pulsar este muestra en la línea de comandos nombre y apellidos que se han introducido, seguidamente cierra el programa.
-
-### Información del repositorio
-Cuando pase a otra versión 0.01x -> 0.02x o 0.1x se creará otro branch distinto para diferenciar entre versiones.  
-Solo se hará si hay grandes cambios que afecten significativamente.  
-Ejemplo: 0.01 -> 0.02, 0.3 -> 0.5
+El branch 0.02 es una mejora significativa respecto a la version 0.01.  
+Tiene los botones aceptar, para abrir una nueva ventana y mostrar la información que se introdujo en los textfields y el botón limpiar para resetear los textfields.
--- programa.java
+++ programa.java
@@ -8,10 +8,12 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.text.Font;
@@ -26,6 +28,7 @@ public class programa extends Application
     @Override
     public void start(Stage stage)
     {
+        //Titulo de la ventana
         stage.setTitle("TP6 ED");
         stage.show();
         GridPane grid = new GridPane();
@@ -34,44 +37,90 @@ public class programa extends Application
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
+        //Tamaño de la ventana
         Scene scene = new Scene(grid, 400, 375);
         stage.setScene(scene);
 
+        //Titulo en la ventana
         Text titulo = new Text("Bienvenido");
+        //fuente
         titulo.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(titulo, 0, 0, 2, 1);
 
+        //Texto del label y lo agrego al grid para que aparezca Nombre:
         Label nombre = new Label("Nombre:");
         grid.add(nombre, 0, 1);
 
         TextField texto = new TextField();
         grid.add(texto, 1, 1);
+        //Informacion cuando pongo raton encima
         texto.setTooltip(new Tooltip("Escribe tu nombre"));
 
+        //Texto del label y lo agrego al grid para que aparezca Apellidos:
         Label ap = new Label("Apellidos:");
         grid.add(ap, 0, 2);
 
         TextField aptexto = new TextField();
         grid.add(aptexto, 1, 2);
+        //Informacion cuando pongo raton encima
         aptexto.setTooltip(new Tooltip("Escribe tus apellidos"));
 
+        //Boton aceptar
         Button boton = new Button("Aceptar");
         HBox hbBtn = new HBox(10);
+        //Informacion cuando pongo raton encima
+        boton.setTooltip(new Tooltip("Aceptar y salir"));
+        //Posicion en este caso abajo_izquierda
         hbBtn.setAlignment(Pos.BOTTOM_LEFT);
         hbBtn.getChildren().add(boton);
-        grid.add(hbBtn, 1, 4);
-        boton.setTooltip(new Tooltip("Aceptar y salir"));
+        //posicion en el grid
+        grid.add(hbBtn, 0, 4);
+        //posicion desplazada respecto a su posicion en el grid
+        boton.setTranslateX(25);
+        boton.setTranslateY(0);
 
-        final Text accion = new Text();
-        grid.add(accion, 1, 6);
+        //Boton limpiar
+        Button limpiar = new Button("Limpiar");
+        HBox hbboton = new HBox(5);
+        //Posicion en este caso abajo_derecha
+        hbboton.setAlignment(Pos.BOTTOM_RIGHT);
+        hbboton.getChildren().add(limpiar);
+        //posicion en el grid
+        grid.add(hbboton,1,4);
+        //posicion desplazada respecto a su posicion en el grid
+        limpiar.setTranslateX(-45);
+        limpiar.setTranslateY(0);
+        //Informacion cuando pongo raton encima
+        limpiar.setTooltip(new Tooltip("Limpiar"));
 
+        //Accion del boton limpiar
+        limpiar.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent e){
+                texto.clear();
+                aptexto.clear();
+            }
+
+        });
+
+        //Accion del boton aceptar
         boton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                accion.setFill(Color.FIREBRICK);
-                accion.setText("Mira la linea de comandos.");
-                System.out.println("Nombre: " + texto.getText()+" "+aptexto.getText());
-                Platform.exit();
+            public void handle(ActionEvent e){
+                //Nueva ventana
+                Stage stage = new Stage();
+                //Titulo de la ventana
+                stage.setTitle("Informaci\u00F3n");
+                GridPane grid = new GridPane();
+                //Tamaño de la ventana
+                stage.setScene(new Scene(grid, 400, 375));
+                stage.show();
+
+                //Texto
+                Text persona = new Text();
+                //Contenido del texto, en este caso getter de la informacion 
+                //que se haya puesto en los textfields de nombre y apellidos
+                persona.setText(texto.getText()+" "+aptexto.getText());
+                //añadir ese texto en el grid
+                grid.add(persona, 0, 1);
             }
         });
     }
